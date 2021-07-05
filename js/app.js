ed19194ba50853278cf9642e3749549eacbef09c
ReemKhalil18499 <khalil.reem18499@gmail.com>
2021-07-05 22:03:30 +0300

    new

--- js/app.js
+++ js/app.js
@@ -9,16 +9,19 @@ let Name=prompt("what is your name?");
 alert("Welcome"+ Name);
 
 let favoritecolor=prompt('is my favorite color pink?');
+favoritecolor=favoritecolor.toLowerCase(); 
+
  if(favoritecolor==='yes'){
      console .log('correct!');
      alert('correct my name is Reem!');
  } else {
      console .log('try again ! its yes!');
      alert('Wrong! , My name is Reem');
-     favoritecolor=favoritecolor.toLowerCase();
+     
 
  } 
  let school=prompt('did i study in LTUC?');
+ school=school.toLowerCase(); 
  if(school==='yes'){
     console .log('correct!');
     alert('Correct!, i Studied in LTUC!');
@@ -27,6 +30,7 @@ let favoritecolor=prompt('is my favorite color pink?');
     alert('Wrong,i Studied in LTUC! ');
 }
 let age=prompt('am i 22 years old?');
+age=age.toLowerCase(); 
  if(age==='yes'){
      console .log('correct!');
      alert('Correct i am 22 years old  ');
@@ -37,6 +41,7 @@ let age=prompt('am i 22 years old?');
 
      
      let live=prompt('Do i live in amman?');
+     live=live.toLowerCase(); 
  if(live==='yes'){
     console .log('correct!');
     alert('Correct , i live in amman ! ');
@@ -45,6 +50,7 @@ let age=prompt('am i 22 years old?');
     alert('Wrong, i live in ! ');
 }
 let happy=prompt('am i happy?');
+happy=happy.toLowerCase(); 
  if(happy==='yes'){
     console .log('correct!');
     alert('Correct i am happy hope the same for you! ');
