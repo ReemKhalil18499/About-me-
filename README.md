677b750bc65daeb923b39d70b7c404489d80081f
ReemKhalil18499 <khalil.reem18499@gmail.com>
2021-07-05 17:53:08 +0300

    new

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # About-me-
+![hello]("https://images-na.ssl-images-amazon.com/images/I/31F9rIvuSqL.png")
---
+++ js/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+
+
+
+// 
+
+let Name=prompt("what is your name?");
+alert("Welcome"+ Name);
+
+let favoritecolor=prompt('is my favorite color pink?');
+ if(favoritecolor==='yes'){
+     console .log('correct!');
+ } else if(favoritecolor==='blue'){
+     console .log('try again ! its yes!');
+     favoritecolor=favoritecolor.toLowerCase();
+
+ } 
+ let school=prompt('did i study in LTUC?');
+ if(school==='yes'){
+    console .log('correct!');
+} else if(school==='no'){
+    console .log('try again ! its yes!');
+}
+let age=prompt('am i 22 years old?');
+ if(age==='yes'){
+     console .log('correct!');
+ } else if(age==='no'){
+     console .log('try again ! its yes!');}
+
+
+     
+     let live=prompt('Do i live in amman?');
+ if(live==='yes'){
+    console .log('correct!');
+} else if(live==='no'){
+    console .log('try again ! its yes!');
+}
+let happy=prompt('am i happy?');
+ if(happy==='yes'){
+    console .log('correct!');
+} else if(happy==='no'){
+    console .log('try again ! its yes!');
+}
+
+
+
+
+
