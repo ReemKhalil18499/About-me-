c64cf0c05ede1ff4f0e9770e651dcb2a3b6e1a50
Marzouq Ibrahim Mohammad Jarah <marzooqjarrah@gmail.com>
2021-07-07 14:35:45 +0300

    q6

--- js/app.js
+++ js/app.js
@@ -48,69 +48,80 @@ function question2() {
     else if (favoritecolor === 'no' || favoritecolor === 'n')
         alert('Wrong! , My name is Reem');
 }
- question2();
+question2();
 
 
- function question3() {
+function question3() {
 
- let school=prompt('did i study in LTUC?');
- school=school.toLowerCase(); 
- if(school==='yes'|| school==='y'){
-    alert('Correct!, i Studied in LTUC!');
-    score++;
+    let school = prompt('did i study in LTUC?');
+    school = school.toLowerCase();
+    if (school === 'yes' || school === 'y') {
+        alert('Correct!, i Studied in LTUC!');
+        score++;
 
-  } 
-  else if (school==='no'|| school==='n' ){
-    alert('Wrong,i Studied in LTUC! '); 
-  }
- }
- question3();
-
-function question4(){
-let age=prompt('am i 22 years old?');
-age=age.toLowerCase(); 
- if(age==='yes'|| age==='y' ){
-     console .log('correct!');
-     alert('Correct i am 22 years old  ');
-     score++;
-
- } 
-   else if (age==='no'|| favoritecolor==='n' ){
-     alert('Wrong i am 22 years old  ');
-   }
- }
- question4();
-
- function question5(){
-
-     let live=prompt('Do i live in amman?');
-     live=live.toLowerCase(); 
- if(live==='yes'|| live==='y'){
-    console .log('correct!');
-    alert('Correct , i live in amman ! ');
-    score++;
-
+    }
+    else if (school === 'no' || school === 'n') {
+        alert('Wrong,i Studied in LTUC! ');
+    }
 }
- else if (live ==='no'|| live==='n' ){
+question3();
 
-    alert('Wrong, i live in amman ! ');
+function question4() {
+    let age = prompt('am i 22 years old?');
+    age = age.toLowerCase();
+    if (age === 'yes' || age === 'y') {
+        console.log('correct!');
+        alert('Correct i am 22 years old  ');
+        score++;
+
+    }
+    else if (age === 'no' || favoritecolor === 'n') {
+        alert('Wrong i am 22 years old  ');
+    }
 }
- }
- question5();
+question4();
+
+function question5() {
+
+    let live = prompt('Do i live in amman?');
+    live = live.toLowerCase();
+    if (live === 'yes' || live === 'y') {
+        console.log('correct!');
+        alert('Correct , i live in amman ! ');
+        score++;
+
+    }
+    else if (live === 'no' || live === 'n') {
+
+        alert('Wrong, i live in amman ! ');
+    }
+}
+question5();
 
 
 
-// let happy=prompt('am i happy?');
-// happy=happy.toLowerCase(); 
-//  if(happy==='yes'|| happy==='y'){
-//     console .log('correct!');
-//     alert('Correct i am happy hope the same for you! ');
-//     score++;
+function question6() {
 
-// } else if (happy==='no'|| happy==='n' ) {
+    let happy = prompt('am i happy?');
+    happy = happy.toLowerCase();
+    if (happy === 'yes' || happy === 'y') {
+        console.log('correct!');
+        alert('Correct i am happy hope the same for you! ');
+        score++;
 
-//     alert('Wrong, i am happy ! ');
-// }
+    } else if (happy === 'no' || happy === 'n') {
+
+        alert('Wrong, i am happy ! ');
+    }
+}
+question6();
+
+
+
+
+
+
+
 
 let food = ['mansaf', 'sushi', 'kbsa', 'kfta']
 let food1 = '';
