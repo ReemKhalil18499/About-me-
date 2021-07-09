ae2d2354f19812db2fd6b04e262544058bdb1ea3
ReemKhalil18499 <khalil.reem18499@gmail.com>
2021-07-09 13:04:07 +0300

    new

--- js/app.js
+++ js/app.js
@@ -1,258 +1,254 @@
 'use strict';
 
 
+// let Name=prompt("what is your name?");
+// alert("Welcome"+  Name);
 
+// let score=0;
 
+// function question1() {
+// let ans=prompt('Guess My favorite Number From 1/5')
+// for(let i = 2; i > -1; i--){
+// if(ans==='4'){
+// alert('Correct! Its 4 !')
+// score++;
+// break;
+// }
+// else if (ans<'4'){
+//     alert("its too low , Try again !")
+//     ans=prompt('Guess My favorite Number From 1/5')
+// }
+// else if(ans>'4'){
+// alert("its too high , please try again !")
+// ans=prompt('Guess My favorite Number From 1/5')
+// }
+// if(i===0){
+//     alert("You are out of tries the answer was 4! ")
+// }
+// }
+// }
+// question1();
 
+// function question2() {
+// let favoritecolor=prompt('is my favorite color pink?');
+// favoritecolor=favoritecolor.toLowerCase(); 
 
-let Name=prompt("what is your name?");
-alert("Welcome"+  Name);
-
-let score=0;
-
-function question1() {
-let ans=prompt('Guess My favorite Number From 1/5')
-for(let i = 2; i > -1; i--){
-if(ans==='4'){
-alert('Correct! Its 4 !')
-score++;
-break;
-}
-else if (ans<'4'){
-    alert("its too low , Try again !")
-    ans=prompt('Guess My favorite Number From 1/5')
-}
-else if(ans>'4'){
-alert("its too high , please try again !")
-ans=prompt('Guess My favorite Number From 1/5')
-}
-if(i===0){
-    alert("You are out of tries the answer was 4! ")
-}
-}
-}
-question1();
-
-function question2() {
-let favoritecolor=prompt('is my favorite color pink?');
-favoritecolor=favoritecolor.toLowerCase(); 
-
-if(favoritecolor==='yes'|| favoritecolor==='y' ){
+// if(favoritecolor==='yes'|| favoritecolor==='y' ){
     
-     alert('correct my name is Reem!');
-     score++;
+//      alert('correct my name is Reem!');
+//      score++;
 
- } 
- else if (favoritecolor==='no'|| favoritecolor==='n' )
-     alert('Wrong! , My name is Reem');
+//  } 
+//  else if (favoritecolor==='no'|| favoritecolor==='n' )
+//      alert('Wrong! , My name is Reem');
      
-}
-     question2();   
+// }
+//      question2();   
 
-     function question3() {
- let school=prompt('did i study in LTUC?');
- school=school.toLowerCase(); 
- if(school==='yes'|| school==='y'){
-    alert('Correct!, i Studied in LTUC!');
-    score++;
+//      function question3() {
+//  let school=prompt('did i study in LTUC?');
+//  school=school.toLowerCase(); 
+//  if(school==='yes'|| school==='y'){
+//     alert('Correct!, i Studied in LTUC!');
+//     score++;
 
-  } 
-  else if (school==='no'|| school==='n' ){
-    alert('Wrong,i Studied in LTUC! '); 
-  }
-}
-  question3();
-  function question4() {
-let age=prompt('am i 22 years old?');
-age=age.toLowerCase(); 
- if(age==='yes'|| age==='y' ){
-     alert('Correct i am 22 years old  ');
-     score++;
+//   } 
+//   else if (school==='no'|| school==='n' ){
+//     alert('Wrong,i Studied in LTUC! '); 
+//   }
+// }
+//   question3();
+//   function question4() {
+// let age=prompt('am i 22 years old?');
+// age=age.toLowerCase(); 
+//  if(age==='yes'|| age==='y' ){
+//      alert('Correct i am 22 years old  ');
+//      score++;
 
- } 
-   else if (age==='no'|| favoritecolor==='n' ){
-     alert('Wrong i am 22 years old  ');
-   }
-  }
-  question4();
+//  } 
+//    else if (age==='no'|| favoritecolor==='n' ){
+//      alert('Wrong i am 22 years old  ');
+//    }
+//   }
+//   question4();
      
-   function question5() {
-     let live=prompt('Do i live in amman?');
-     live=live.toLowerCase(); 
- if(live==='yes'|| live==='y'){
-    alert('Correct , i live in amman ! ');
-    score++;
+//    function question5() {
+//      let live=prompt('Do i live in amman?');
+//      live=live.toLowerCase(); 
+//  if(live==='yes'|| live==='y'){
+//     alert('Correct , i live in amman ! ');
+//     score++;
 
-}
- else if (live ==='no'|| live==='n' ){
+// }
+//  else if (live ==='no'|| live==='n' ){
     
-    alert('Wrong, i live in amman ! ');
-}
-   }
-question5();
+//     alert('Wrong, i live in amman ! ');
+// }
+//    }
+// question5();
 
-function question6() {
-let happy=prompt('am i happy?');
- if(happy==='yes'|| happy==='y'){
-    alert('Correct i am happy hope the same for you! ');
-    score++;
+// function question6() {
+// let happy=prompt('am i happy?');
+//  if(happy==='yes'|| happy==='y'){
+//     alert('Correct i am happy hope the same for you! ');
+//     score++;
 
-} else if (happy==='no'|| happy==='n' ) {
+// } else if (happy==='no'|| happy==='n' ) {
   
-    alert('Wrong, i am happy ! ');
-}
-}
-question6();
+//     alert('Wrong, i am happy ! ');
+// }
+// }
+// question6();
 
-function question7() {
-let food=['mansaf','sushi','kbsa','kfta'];
-let food1=prompt("what is my favorite food ?")
-let food2=false
-for (let i = 5;i > -1;i--){ 
-for (let j = 0; j <food.length ;j++){
-    if (food[j]===food1) {
-        alert("yes you are correct , here are all the answers"+ food);
-        food2=true
-        score++;
-        break;
-    }
-    }
-    alert("it is wrong please Try again !")
-    food1=prompt("what is my favorite food ?")
-}
-}
-question7();
-alert('you have a score of '+ score+'/7');
-
-
-function question1() {
-    let ans = prompt('Guess My favorite Number From 1/5')
-    for (let i = 0; i < 4; i++)
-        if (ans === '4') {
-            alert('Correct! Its 4 !')
-            score++;
-            break;
-        }
-        else if (ans < '4') {
-            alert("its too low , Try again !")
-            ans = prompt('Guess My favorite Number From 1/5')
-
-        }
-        else if (ans > '4') {
-            alert("its too high , please try again !")
-            ans = prompt('Guess My favorite Number From 1/5')
-        }
-
-}
-question1();
+// function question7() {
+// let food=['mansaf','sushi','kbsa','kfta'];
+// let food1=prompt("what is my favorite food ?")
+// let food2=false
+// for (let i = 5;i > -1;i--){ 
+// for (let j = 0; j <food.length ;j++){
+//     if (food[j]===food1) {
+//         alert("yes you are correct , here are all the answers"+ food);
+//         food2=true
+//         score++;
+//         break;
+//     }
+//     }
+//     alert("it is wrong please Try again !")
+//     food1=prompt("what is my favorite food ?")
+// }
+// }
+// question7();
+// alert('you have a score of '+ score+'/7');
 
 
-function question2() {
+// function question1() {
+//     let ans = prompt('Guess My favorite Number From 1/5')
+//     for (let i = 0; i < 4; i++)
+//         if (ans === '4') {
+//             alert('Correct! Its 4 !')
+//             score++;
+//             break;
+//         }
+//         else if (ans < '4') {
+//             alert("its too low , Try again !")
+//             ans = prompt('Guess My favorite Number From 1/5')
+
+//         }
+//         else if (ans > '4') {
+//             alert("its too high , please try again !")
+//             ans = prompt('Guess My favorite Number From 1/5')
+//         }
+
+// }
+// question1();
 
 
-    let favoritecolor = prompt('is my favorite color pink?');
-    favoritecolor = favoritecolor.toLowerCase();
-
-    if (favoritecolor === 'yes' || favoritecolor === 'y') {
-
-        alert('correct my name is Reem!');
-        score++;
-
-    }
-    else if (favoritecolor === 'no' || favoritecolor === 'n')
-        alert('Wrong! , My name is Reem');
-}
-question2();
+// function question2() {
 
 
-function question3() {
+//     let favoritecolor = prompt('is my favorite color pink?');
+//     favoritecolor = favoritecolor.toLowerCase();
 
-    let school = prompt('did i study in LTUC?');
-    school = school.toLowerCase();
-    if (school === 'yes' || school === 'y') {
-        alert('Correct!, i Studied in LTUC!');
-        score++;
+//     if (favoritecolor === 'yes' || favoritecolor === 'y') {
 
-    }
-    else if (school === 'no' || school === 'n') {
-        alert('Wrong,i Studied in LTUC! ');
-    }
-}
-question3();
+//         alert('correct my name is Reem!');
+//         score++;
 
-function question4() {
-    let age = prompt('am i 22 years old?');
-    age = age.toLowerCase();
-    if (age === 'yes' || age === 'y') {
-        console.log('correct!');
-        alert('Correct i am 22 years old  ');
-        score++;
-
-    }
-    else if (age === 'no' || favoritecolor === 'n') {
-        alert('Wrong i am 22 years old  ');
-    }
-}
-question4();
-
-function question5() {
-
-    let live = prompt('Do i live in amman?');
-    live = live.toLowerCase();
-    if (live === 'yes' || live === 'y') {
-        console.log('correct!');
-        alert('Correct , i live in amman ! ');
-        score++;
-
-    }
-    else if (live === 'no' || live === 'n') {
-
-        alert('Wrong, i live in amman ! ');
-    }
-}
-question5();
+//     }
+//     else if (favoritecolor === 'no' || favoritecolor === 'n')
+//         alert('Wrong! , My name is Reem');
+// }
+// question2();
 
 
+// function question3() {
 
-function question6() {
+//     let school = prompt('did i study in LTUC?');
+//     school = school.toLowerCase();
+//     if (school === 'yes' || school === 'y') {
+//         alert('Correct!, i Studied in LTUC!');
+//         score++;
 
-    let happy = prompt('am i happy?');
-    happy = happy.toLowerCase();
-    if (happy === 'yes' || happy === 'y') {
-        console.log('correct!');
-        alert('Correct i am happy hope the same for you! ');
-        score++;
+//     }
+//     else if (school === 'no' || school === 'n') {
+//         alert('Wrong,i Studied in LTUC! ');
+//     }
+// }
+// question3();
 
-    } else if (happy === 'no' || happy === 'n') {
+// function question4() {
+//     let age = prompt('am i 22 years old?');
+//     age = age.toLowerCase();
+//     if (age === 'yes' || age === 'y') {
+//         console.log('correct!');
+//         alert('Correct i am 22 years old  ');
+//         score++;
 
-        alert('Wrong, i am happy ! ');
-    }
-}
-question6();
+//     }
+//     else if (age === 'no' || favoritecolor === 'n') {
+//         alert('Wrong i am 22 years old  ');
+//     }
+// }
+// question4();
+
+// function question5() {
+
+//     let live = prompt('Do i live in amman?');
+//     live = live.toLowerCase();
+//     if (live === 'yes' || live === 'y') {
+//         console.log('correct!');
+//         alert('Correct , i live in amman ! ');
+//         score++;
+
+//     }
+//     else if (live === 'no' || live === 'n') {
+
+//         alert('Wrong, i live in amman ! ');
+//     }
+// }
+// question5();
 
 
-function question7() {
-    let food2;
-    let food = ['mansaf', 'sushi', 'kbsa', 'kfta']
-    let food1 = prompt("what is my favorite food ?")
-     food2 = false;
-    for (let i = 5; i > -1; i--) {
-        for (let j = 0; j < food.length; j++) {
-            console.log(food[j]);
-            if (food[j] === food1) {
-                alert("yes you are correct , here are all the answers" + food);
-                 food2 = true; 
-                score++;
-                break;
-            }
-        }
-        alert("it is wrong please Try again !");
-        food1 = prompt("what is my favorite food ?")
+
+// function question6() {
+
+//     let happy = prompt('am i happy?');
+//     happy = happy.toLowerCase();
+//     if (happy === 'yes' || happy === 'y') {
+//         console.log('correct!');
+//         alert('Correct i am happy hope the same for you! ');
+//         score++;
+
+//     } else if (happy === 'no' || happy === 'n') {
+
+//         alert('Wrong, i am happy ! ');
+//     }
+// }
+// question6();
+
+
+// function question7() {
+//     let food2;
+//     let food = ['mansaf', 'sushi', 'kbsa', 'kfta']
+//     let food1 = prompt("what is my favorite food ?")
+//      food2 = false;
+//     for (let i = 5; i > -1; i--) {
+//         for (let j = 0; j < food.length; j++) {
+//             console.log(food[j]);
+//             if (food[j] === food1) {
+//                 alert("yes you are correct , here are all the answers" + food);
+//                  food2 = true; 
+//                 score++;
+//                 break;
+//             }
+//         }
+//         alert("it is wrong please Try again !");
+//         food1 = prompt("what is my favorite food ?")
             
-        }
+//         }
 
-    }
-    question7();
-    alert('you have a score of ' + score + '/7');
+//     }
+//     question7();
+//     alert('you have a score of ' + score + '/7');
 
 
